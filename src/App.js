b39e7b1fe381aca25e710630a457a7c524a0e9f2
Matias <mpintov42@gmail.com>
2022-04-17 21:00:18 -0400

    added props in cards

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from './components/Hero'
 import Card from './components/Card'
+import katieImage from './images/katie-zaferes.png'
 
 export default function App(){
   return (
     <div>
       <Navbar/>
       <Hero/>
-      <Card/>
+      <Card img={katieImage} rating="5.0" reviewCount={6} country="USA" title="Life Lessons with Katie Zaferes" price={136}/>
     </div>
   )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import katieImage from '../images/katie-zaferes.png'
+
 import starCard from '../images/star.png'
 
-export default function Card(){
+export default function Card(props){
     return(
         <div className='card'>
-            <img src={katieImage} alt="Katie" />
+            <img src={props.img} alt="Katie" />
             <div className='bodyCard'>
-                <p> <img src={starCard} alt='star'/> 5.0 <span className='grayText'>(6)·USA</span></p>
-                <p>Life lessons with Katie Zaferes</p>
-                <p><span className='boldText'>From $136 </span>/ person</p>
+                <p> <img src={starCard} alt='star'/>{props.rating}<span className='grayText'>({props.reviewCount})·{props.country}</span></p>
+                <p>{props.title}</p>
+                <p><span className='boldText'>From ${props.price1} </span>/ person</p>
             </div>
         </div>
     )
